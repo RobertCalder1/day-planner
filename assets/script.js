6247c23a1d9d23161fe362b2d606f9e99ae06e26
RobertCalder1 <robertcalder27@hotmail.com>
2021-09-23 12:34:51 +1000

    added localStorage functions and time based color coding

--- assets/script.js
+++ assets/script.js
@@ -4,4 +4,26 @@ $("#currentDay").text(today.format("dddd, MMMM Do"));
 $(".saveBtn").on("click", function () {
   var id = $(this).attr("id");
   var userEntry = $(this).siblings("textarea").val();
+  localStorage.setItem(id, userEntry);
 });
+var currentTime = moment().hours();
+
+for (let i = 9; i <= 17; i++) {
+  var getStorage = localStorage.getItem(i);
+  $("#" + i)
+    .siblings("textarea")
+    .val(getStorage);
+  if (i > currentTime) {
+    $("#" + i)
+      .siblings("textarea")
+      .addClass("future");
+  } else if (i === currentTime) {
+    $("#" + i)
+      .siblings("textarea")
+      .addClass("present");
+  } else {
+    $("#" + i)
+      .siblings("textarea")
+      .addClass("past");
+  }
+}
