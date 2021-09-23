a88c6b390a6295719b749b62a623da71da88b6b9
RobertCalder1 <robertcalder27@hotmail.com>
2021-09-23 12:24:59 +1000

    added text areas and fixed buttons

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,7 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+$(".saveBtn").on("click", function () {
+  var id = $(this).attr("id");
+  var userEntry = $(this).siblings("textarea").val();
+});
